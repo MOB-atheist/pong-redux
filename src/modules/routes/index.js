b9ea46d3765bd0390d369fa428274a89cf94fd4c
Pablo Justino (Maxi) <pabskne@gmail.com>
2021-04-19 17:06:37 -0300

    gh-pages Routes adaptation

--- src/modules/routes/index.js
+++ src/modules/routes/index.js
@@ -5,17 +5,19 @@ import Home from '../../pages/Home'
 import User from '../../pages/User'
 import NotFound from '../../pages/NotFound'
 
+const Alias = 'pong-redux'
+
 export default function Routes(props) {
     return (
         <>
             <Switch>
-                <Route exact path="/">
+                <Route exact path={"/"+Alias}>
                     <Home />
                 </Route>
-                <Route exact path="/user">
+                <Route exact path={"/"+Alias+"/user"}>
                     <User />
                 </Route>
-                <Route path="/">
+                <Route path={"/"+Alias+""}>
                     <NotFound />
                 </Route>
             </Switch>
