5452f34004f2a59702479362492e4928db975ee0
Pablo Justino (Maxi) <pabskne@gmail.com>
2021-04-29 09:51:27 -0300

    Hash Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
+import { HashRouter } from 'react-router-dom'
+
 // Redux implementation
 import { Provider } from 'react-redux'
 import { store, persistor } from './app/store'
@@ -69,7 +71,9 @@ ReactDOM.render(
             <Provider store={store}>
                 <PersistGate persistor={persistor}>
                     <Header />
-                    <App />
+                    <HashRouter>
+                        <App />
+                    </HashRouter>
                     <Footer />
                 </PersistGate>
             </Provider>
